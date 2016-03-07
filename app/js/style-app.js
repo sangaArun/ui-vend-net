5418a6f8dcf8368ff0ddb884ade6b7eff8225942
ArunSanga <sanga2000@gmail.com>
2016-03-07 23:06:22 +0530

    Added leftnav toggle animations

--- app/js/style-app.js
+++ app/js/style-app.js
@@ -8,6 +8,6 @@ $(function() {
       $("#menu-toggle").click(function(e) {
             console.log('toggle');
             e.preventDefault();
-            $("#wrapper").toggleClass("toggled");
+            $("#wrapper1").toggleClass("toggled");
         });
 });
